e599b429d897107bfe86d94ffe09e1c4ba97e382
Victor <titotortuga3@gmail.com>
2023-02-21 01:46:43 -0300

    Projeto Playstation Store finalizado!

---
+++ store/src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css'],
+})
+export class CardPricingComponent implements OnInit {
+  @Input()
+  gameType: string = 'Digital PS4';
+  @Input()
+  gamePrice: string = 'US$ 89,90';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
